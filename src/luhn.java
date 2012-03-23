ff19b56ad11c4140c1f6d1054c78f15ced418085
Nicola <nicola_pi@hotmail.it>
2012-03-23 21:29:19 +0100

    New methods implemented in luhn class and new test

--- src/luhn.java
+++ src/luhn.java
@@ -1,5 +1,10 @@
 
 public class luhn {
+	/**
+	Calculate the Luhn sum of the input number
+	@param str number to check
+	@return sum calcuted with Luhn algorithm
+	**/
 	public static int luhnTest(String str){
 		int sum=0;
 		boolean isEven=false;
@@ -17,9 +22,35 @@ public class luhn {
 		return sum;
 	}
 	
+	/**
+	Return true if the input number is valid according to the Luhn formula
+	@param str number to check
+	**/
 	public static boolean isLuhnValid(String str){
 		if (luhnTest(str)%10==0)
 			return true;
 		return false;	
+	}
+	
+	/**
+	Return the check digit that makes the input number valid according to the Luhn formula
+	@param str number to check
+	@return calculated check digit
+	**/
+	public static int getCheckDigit(String str){
+		int k=luhnTest(str+"0");
+		int i=0;
+		if(k%10!=0)
+			i=10-k%10;
+		return i;
+	}
+	
+	/**
+	Return the input number with check digit appended
+	@param str number to make Luhn valid
+	@return Luhn valid number
+	**/
+	public static String getLuhnNumber(String str){
+		return str+getCheckDigit(str);
 	}
 }
--- src/test.java
+++ src/test.java
@@ -3,13 +3,15 @@
 public class test {
 
 	public static void main(String[] args) {
-		System.out.println("#Luhm algorithm test#");
-		
-		if(luhn.isLuhnValid("12345674"))
-			System.out.println("Valid code");
+		//Example
+		String code="573829491";
+		System.out.println("Input number: "+code);
+		if(luhn.isLuhnValid(code))
+			System.out.println("The number is valid according to the Luhn formula. Luhn sum:"+luhn.luhnTest(code));
 		else
-			System.out.println("Invalid code");
-	}
-
-	
+			System.out.println("The number is not valid according to the Luhn formula. Luhn sum:"+luhn.luhnTest(code));
+	    System.out.println("The check digit to append is: "+luhn.getCheckDigit(code));
+	    String validcode=luhn.getLuhnNumber(code);
+	    System.out.println("The Luhn valid number is: "+validcode);
+	    System.out.println("The calculated Luhn number is valid: "+luhn.isLuhnValid(validcode));	}
 }
